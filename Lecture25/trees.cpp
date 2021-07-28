8b48e3321603277ef7eb5688d5b46b71b3cb5cdd
mosina916 <mosinaashraf1913@gmail.com>
2021-07-28 22:09:23 +0530

    trees

--- Lecture25/trees.cpp
+++ Lecture25/trees.cpp
@@ -115,6 +115,84 @@ int countnodes(node*root){
 
 
 }
+int heightoftree(node*root){
+
+	// base case
+	if(root==NULL){
+		return 0;
+	}
+
+
+
+	// recursive case
+
+	int lsth=heightoftree(root->left); //3
+	int rsth=heightoftree(root->right); //3
+	return max(lsth,rsth)+1;
+
+}
+
+int sumofnodesoftree(node*root){
+
+
+	// base case
+	if(root==NULL){
+		return 0;
+	}
+
+
+	// recursive case
+	int lstsum=sumofnodesoftree(root->left);
+	int rstsum=sumofnodesoftree(root->right);
+
+
+	return lstsum+rstsum+root->data;
+
+
+
+}
+
+void mirroroftree(node*root){
+	// base case
+	if(root==NULL){
+		return;
+	}
+
+	// recursive case
+	swap(root->left,root->right);
+	mirroroftree(root->left);
+	mirroroftree(root->right);
+
+
+
+}
+
+int diameter(node*root){
+
+	// base case
+	if(root==NULL){
+		return 0;
+	}
+
+
+	// recursive case
+
+	// op1
+	int op1=diameter(root->left);
+
+
+	// op2
+	int op2=diameter(root->right);
+
+
+	// op3
+	int op3=heightoftree(root->left)+heightoftree(root->right);
+
+	return max(op1,max(op2,op3));
+}
+
+
+
 // i/p  8 3 1 -1 -1 6 4 -1 -1 7 -1 -1 10 -1 14 13 -1 -1 -1
 
 
@@ -141,6 +219,25 @@ int main(){
 
 
 	cout<<"Total nodes present in tree are : "<<countnodes(root)<<endl;
+
+	cout<<"height of tree is : "<<heightoftree(root)<<endl;
+
+	cout<<"sum of nodes of tree is : "<<sumofnodesoftree(root)<<endl;
+
+	// mirroroftree(root);
+	// cout<<"preorderprint after mirroring is : "<<endl;
+
+	// preorderprint(root);
+
+	// cout<<endl;
+
+
+		cout<<"Diameter of tree is : "<<diameter(root)<<endl;
+
+
+
+
+
 
 
 
